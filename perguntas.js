a0ef46fed6ea35eaff9715e2b4dc7eef0a55856f
Caua Otavio <caua.araujo.lima@escola.pr.gov.br>
2025-10-14 00:13:59 +0000

    Bahhhh

---
+++ perguntas.js
@@ -0,0 +1,77 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+
+criaCartao(
+    'Matámatica',
+    'Qual é a fórmula do teorema de pitágoras?',
+    'a² + b² = c²'
+)
+
+criaCartao(
+    'Português',
+    'Quais são os 4 tipos de porquê?',
+    'por que, porque, por quê e porquê'
+)
+
+criaCartao(
+    'História',
+    'Quais eram os povos da mesopotâmia?',
+    'Sumérios, Acádios, Amoritas, Assírios e Caldeus'
+)
+
+criaCartao(
+    'Biologia',
+    'Quais são os 5 reinos propostos por Whittaker? ',
+    'Monera, Protista, Fungi, Plantae e Animalia'
+)
+
+criaCartao(
+    'Física',
+    'Quais são as 5 áreas da física?',
+    'Mecânica, Termologia, Óptica, Ondulatória e Eletromagnetismo'
+)
+
+criaCartao(
+    'Sociológia',
+    'A sociologia é uma ciência humana que estuda a sociedade. Das opções abaixo, a que não contempla um de seus objetivos é',
+    'Compreender a existência humana e o saber por meio da análise racional relacionada com a história.'
+)
+
+criaCartao(
+    'Projeto de Vida',
+    'Como contribuir para o ambiente que eu vivo?',
+    'Cuidar dos outros e ser uma boa pessoa com os outros'
+)
+
+criaCartao(
+    'Educação Financeira',
+    'O que é CDI?',
+    'CDI significa Certificado de Depósito Interbancário. É um título de empréstimo de curtíssimo prazo, geralmente de um dia útil, negociado entre bancos para que eles possam regularizar seus saldos diários de caixa. A média da taxa de juros dessas operações é a Taxa DI, que serve como um importante indicador de rentabilidade para diversos investimentos de renda fixa, como CDBs, LCIs e LCAs. '
+)
+
+criaCartao(
+    'Química',
+    'O que é o elemento quimico Fe?',
+    'Ferro'
+)
